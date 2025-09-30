d603f223b90e2cfb6e0fd8cc4eaf6218ed7700a5
yamandogus <yamandogus251@gmail.com>
2025-10-01 00:11:49 +0300

    update welcome text and step instructions

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,13 +18,13 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome!fasdf fasdfas fsadfa</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Editeawf ewf awef <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
